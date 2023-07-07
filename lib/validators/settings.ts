d48894f44b79e19916d5a0f948697a64125508c2
Ricardo Chu <92066360+kypexfly@users.noreply.github.com>
2023-07-06 20:55:25 -0500

    fix: validate website is an url

--- lib/validators/settings.ts
+++ lib/validators/settings.ts
@@ -4,7 +4,7 @@ export const SettingsUpdateValidator = z.object({
   username: z.string().min(3).max(32).regex(/^[a-zA-Z0-9_]+$/).optional(),
   bio: z.string().max(200).optional(),
   location: z.string().max(100).optional(),
-  website: z.string().max(200).optional(),
+  website: z.string().max(200).url().optional(),
 })
 
 export type SettingUpdateRequest = z.infer<typeof SettingsUpdateValidator>
