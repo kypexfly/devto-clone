64bc0401ac5bd04035b29fe541841abec97195ab
Ricardo Chu <92066360+kypexfly@users.noreply.github.com>
2023-11-08 20:57:22 -0500

    fix: empty base url in axios instance

--- lib/api/axiosInstance.ts
+++ lib/api/axiosInstance.ts
@@ -1,5 +1,3 @@
 import axios from "axios"
 
-export const axiosInstance = axios.create({
-  baseURL: process.env.NEXT_PUBLIC_API_URL ?? "http://localhost:3000",
-})
+export const axiosInstance = axios.create()
