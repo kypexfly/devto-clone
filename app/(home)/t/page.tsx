5de96a80118cc0c6ecfd155fa5c033d132560688
Ricardo Chu <92066360+kypexfly@users.noreply.github.com>
2023-11-12 22:36:55 -0500

    feat: adjust padding

--- app/(home)/t/page.tsx
+++ app/(home)/t/page.tsx
@@ -27,10 +27,10 @@ export default async function TagsPage() {
 
   return (
     <div className="mx-auto w-full max-w-screen-md">
-      <h1 className="mb-3 scroll-m-20 px-2 pt-4 text-3xl font-bold tracking-tight lg:text-4xl">
+      <h1 className="mb-3 scroll-m-20 px-4 pt-4 text-3xl font-bold tracking-tight lg:text-4xl">
         Most popular tags
       </h1>
-      <ul className="flex flex-wrap items-center gap-3">
+      <ul className="flex flex-wrap items-center gap-3 px-4">
         {tags.map((tag) => (
           <li key={tag.id}>
             <Link href={`/t/${tag.name}`}>
